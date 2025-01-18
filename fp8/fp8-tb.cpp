4bb2e8a8722f23ed0a546f1fca9b1e12c5d19cea
Luis Leon <lleon95@gmail.com>
2025-01-18 22:10:24 +0100

    Add addition operator

--- fp8/fp8-tb.cpp
+++ fp8/fp8-tb.cpp
@@ -3,7 +3,7 @@
 #include "fp8.h"
 
 int main() {
-  
+  /*
   for (float i = -512.f; i < 512.f; i += 0.01f) {
     minifloat val{i};
     float recast = static_cast<float>(val);
@@ -11,6 +11,7 @@ int main() {
               << "Minifloat: " << val << std::endl
               << "Reconverted: " << recast << std::endl << std::endl;
   }
+  */
 
   minifloat val{0.f};
   float recast = static_cast<float>(val);
@@ -30,6 +31,28 @@ int main() {
             << "Minifloat: " << val << std::endl
             << "Reconverted: " << recast << std::endl << std::endl;
 
+  val = minifloat{0.5f} + minifloat{0.1875f};
+  std::cout << "Original: " << minifloat{0.6875} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
 
+  val = minifloat{0.1875f} + minifloat{0.5f};
+  std::cout << "Original: " << minifloat{0.6875} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
+
+  val = minifloat{0.1875f} + minifloat{0.f};
+  std::cout << "Original: " << minifloat{0.1875f} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
+
+  val = minifloat{0.5f} + minifloat{-0.1875f};
+  std::cout << "Original: " << minifloat{0.3125} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
+
+  val = minifloat{0.1875f} + minifloat{-0.5f};
+  std::cout << "Original: " << minifloat{-0.3125} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
+
+  val = minifloat{-0.1875f} + minifloat{0.f};
+  std::cout << "Original: " << minifloat{-0.1875f} << std::endl
+            << "Minifloat: " << val << std::endl << std::endl;
   return 0;
 }
--- fp8/fp8.h
+++ fp8/fp8.h
@@ -7,6 +7,7 @@
 #define FP8_FP8_H_
 
 #include <ap_int.h>
+#include <hls_math.h>
 #include <cstdint>
 
 #ifndef __SYNTHESIS__
@@ -28,6 +29,30 @@ class minifloat {
     this->sign_ = m.sign_;
   }
 
+  /* Addition */
+  minifloat operator+(const minifloat& other) const {
+    return addition(*this, other);
+  }
+
+  /* Substraction */
+  minifloat operator-(const minifloat& other) const {
+    minifloat negative = -other;
+    return minifloat{*this + negative};
+  }
+
+  /* Multiplication */
+  minifloat operator*(const minifloat& other) const {
+    return multiplication(*this, other);
+  }
+
+  /* Unary minus */
+  minifloat operator-() const {
+    minifloat res = *this;
+    res.sign_ = !this->sign_;
+    return res;
+  }
+  
+
   /* Cast to float */
   operator const float() const { return convertfloat(); }
 
@@ -41,6 +66,8 @@ class minifloat {
   ap_uint<1> sign_;
 
   float convertfloat() const noexcept;
+  minifloat addition(const minifloat &a, const minifloat &b) const noexcept;
+  minifloat multiplication(const minifloat &a, const minifloat &b) const noexcept;
 
   const uint8_t kExpOffset = 120; // 127 (32 bits) - 7 (8 bits)
 };
@@ -72,6 +99,8 @@ inline minifloat::minifloat(const float val) noexcept {
   this->sign_ = raw(31, 31);
   this->exponent_ = exponent(3, 0);
   this->mantissa_ = mantissa(22, 20);
+
+  std::cout << "m: " << this->mantissa_ << " e: " << this->exponent_ << " s: " << this->sign_ << std::endl;
 }
 
 inline float minifloat::convertfloat() const noexcept {
@@ -91,6 +120,65 @@ inline float minifloat::convertfloat() const noexcept {
 
   element.raw = raw;
   return element.val;
+}
+
+inline minifloat minifloat::addition(const minifloat &a, const minifloat &b) const noexcept {
+  minifloat res, a_, b_;
+  
+  /* Swap flag: in case the second operand is bigger */
+  ap_uint<1> swap = a.exponent_ < b.exponent_ ?  ap_uint<1>{1} : ap_uint<1>{0};
+  a_ = swap ? b : a; b_ = swap ? a : b;
+
+  /* Define the variables: swap is needed */
+  ap_uint<3> m1 = a_.mantissa_;
+  ap_uint<3> m2 = b_.mantissa_;
+  ap_uint<4> e1 = a_.exponent_;
+  ap_uint<4> e2 = b_.exponent_;
+  ap_uint<1> s1 = a_.sign_;
+  ap_uint<1> s2 = b_.sign_;
+  ap_uint<1> sign = s1 ^ s2; // check sign
+
+  /* Difference between exponents */
+  ap_uint<4> diff_e = e1 - e2;
+
+  std::cout << "N1 m: " << a_.mantissa_ << " e: " << a_.exponent_ << " s: " << a_.sign_ << std::endl;
+  std::cout << "N2 m: " << b_.mantissa_ << " e: " << b_.exponent_ << " s: " << b_.sign_ << std::endl;
+
+  /* Set the (1 + mf): 3 frac for mantissa, 16 bits for integer part */
+  ap_fixed<16 + 3, 16> operand_a{1}, operand_b{1}, operand_c{0};
+  operand_a.V(2, 0) = m1;
+  operand_b.V(2, 0) = m2;
+  operand_a <<= diff_e;
+
+  std::cout << "Diff: " << diff_e << std::endl;
+  std::cout << "Operand A: " << operand_a << std::endl;
+  std::cout << "Operand B: " << operand_b << std::endl;
+
+  /* Compute the number of integer bits */
+  operand_c = operand_a + (sign ?  decltype(operand_b){-operand_b} : decltype(operand_b){operand_b});
+  ap_uint<4> ilog2_c = hls::ilogb(operand_c);
+
+  std::cout << "Operand C: " << operand_c << std::endl;
+  std::cout << "ilog C: " << ilog2_c << std::endl;
+
+  /* Get normalised mantissa */
+  ap_fixed<16 + 3, 16> operand_c_norm = operand_c >> ilog2_c;
+  std::cout << "Operand C Norm: " << operand_c_norm << std::endl;
+
+  /* Get resulting components */
+  res.mantissa_ = operand_c_norm(2, 0);
+  res.exponent_ = ilog2_c + b_.exponent_;
+
+  // TODO(lleon): check accordingly
+  res.sign_ = sign ? a_.sign_ : sign;
+
+  std::cout << "Res m: " << res.mantissa_ << " e: " << res.exponent_ << " s: " << res.sign_ << std::endl;
+
+  return res;
+}
+
+inline minifloat minifloat::multiplication(const minifloat &a, const minifloat &b) const noexcept {
+  return minifloat{0.f};
 }
 
 
